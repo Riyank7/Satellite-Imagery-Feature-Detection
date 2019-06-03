feb938c37425c111acf977d7559fc889b0bb05f2
Riyank Mukhopadhyay <33059184+Riyank7@users.noreply.github.com>
2019-06-03 11:44:47 +0530

    Update tiff_read.py

--- tiff_read.py
+++ tiff_read.py
@@ -81,7 +81,7 @@ def generate_mask_for_image_and_class(raster_size, imageId, class_type, grid_siz
     return mask
 
 
-inDir = './data'
+inDir = './{/content}/competitions/dstl-satellite-imagery-feature-detection'
 
 
 # read the training data from train_wkt_v4.csv
